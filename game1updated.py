d417d6613e19d096804739f8259d139679fceca8
Shailendra Shetty <61897256+MEGATRON03@users.noreply.github.com>
2021-01-31 14:31:02 +0530

    Add files via upload

---
+++ game1updated.py
@@ -0,0 +1,176 @@
+import turtle
+import winsound
+
+
+
+wn=turtle.Screen()
+wn.title("pong")
+wn.bgcolor("black")
+wn.setup(width=800,height=600)
+wn.tracer(0)
+
+
+
+#score
+score_a=0
+score_b=0
+
+
+
+
+
+#paddle a
+paddle_a=turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("maroon")
+paddle_a.shapesize(stretch_wid=5,stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350,0)
+
+
+
+#paddle b
+paddle_b=turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("maroon")
+paddle_b.shapesize(stretch_wid=5,stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350,0)
+
+
+#ball
+ball=turtle.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("green")
+ball.penup()
+ball.goto(0,0)
+
+ball.dx=0.2
+ball.dy=-0.2
+
+
+
+#pen
+pen=turtle.Turtle()
+pen.speed(0)
+pen.color("yellow")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("Player A:0  Player B:0",align="center",font=("courier",24,"normal"))
+
+
+
+#pen 1
+pen1=turtle.Turtle()
+pen1.speed(0)
+pen1.color("red")
+pen1.penup()
+pen1.hideturtle()
+pen1.goto(0,26)
+ 
+if score_a>2:
+ pen1.write("a is the winner",align="center",font=("courier",24,"normal"))
+elif score_b>2:
+ pen1.write("b is the winner",align="center",font=("courier",24,"normal"))
+ 
+ 
+
+
+#function
+def paddle_a_up():
+    y=paddle_a.ycor()
+    y+=20
+    paddle_a.sety(y)
+    
+def paddle_a_down():
+    y=paddle_a.ycor()
+    y-=20
+    paddle_a.sety(y)
+
+
+def paddle_b_up():
+    y=paddle_b.ycor()
+    y+=20
+    paddle_b.sety(y)
+    
+def paddle_b_down():
+    y=paddle_b.ycor()
+    y-=20
+    paddle_b.sety(y)
+
+
+
+
+
+#keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up,"w")
+wn.onkeypress(paddle_a_down,"s")
+
+wn.onkeypress(paddle_b_up,"Up")
+wn.onkeypress(paddle_b_down,"Down")
+
+
+
+#main game loop
+while True :
+ wn.update()
+
+
+ 
+ if score_a>2:
+  print("A is the winner")
+  pen1.write("A is the winner",align="center",font=("courier",24,"normal"))
+  break
+ elif score_b>2:
+  print("B is the winner")
+  pen1.write("B is the winner",align="center",font=("courier",24,"normal"))
+  break
+
+
+
+ #move the ball
+ ball.setx(ball.xcor()+ball.dx)
+ ball.sety(ball.ycor()+ball.dy)
+
+
+ #border checking
+ if ball.ycor()>290:
+     ball.sety(290)
+     ball.dy*=-1
+     winsound.PlaySound("bounce.wav",winsound.SND_ASYNC)
+     
+ if ball.ycor()<-290:
+     ball.sety(-290)
+     ball.dy*=-1
+     winsound.PlaySound("bounce.wav",winsound.SND_ASYNC)
+     
+ if ball.xcor()>390:
+     ball.goto(0,0)
+     ball.dx*=-1
+     score_a+=1
+     pen.clear()
+     pen.write("Player A:{}  Player B:{}".format(score_a,score_b),align="center",font=("courier",24,"normal"))
+
+ if ball.xcor()<-390:
+     ball.goto(0,0)
+     ball.dx*=-1
+     score_b+=1
+     pen.clear()
+     pen.write("Player A:{}  Player B:{}".format(score_a,score_b),align="center",font=("courier",24,"normal"))
+
+
+#paddle and ball collision
+ if (ball.xcor()>340 and ball.xcor()<350) and (ball.ycor()<paddle_b.ycor() + 40 and ball.ycor()>paddle_b.ycor() - 50):
+     ball.setx(340)
+     ball.dx*=-1
+     winsound.PlaySound("bounce.wav",winsound.SND_ASYNC)
+     
+ if (ball.xcor()<-340 and ball.xcor()>-350) and (ball.ycor()<paddle_a.ycor() + 40 and ball.ycor()>paddle_a.ycor() - 50):
+     ball.setx(-340)
+     ball.dx*=-1
+     winsound.PlaySound("bounce.wav",winsound.SND_ASYNC)
